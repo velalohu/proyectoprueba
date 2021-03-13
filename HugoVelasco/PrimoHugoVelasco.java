aefb0c2e20ceb84c4d453686138222f728cda5b0
velalohu <80323798+velalohu@users.noreply.github.com>
2021-03-13 12:44:44 +0100

    Add files via upload

---
+++ HugoVelasco/PrimoHugoVelasco.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primo;
+
+/**
+ *
+ * @author hugo
+ */
+public class Primo {
+
+    public static boolean esPrimo(int num){
+        int contador = 0;
+        for (int i = 1; i < num; i++) {
+            if (num%i == 0){
+                contador++;
+            }
+        }
+        if(contador>1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(esPrimo(150));
+    }
+    
+}
